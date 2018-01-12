db5d1b45e7c4661c9bf3d7274f6e291a5725368c
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-01-12 18:30:02 +0100

    build and export

---
+++ docs/_next/ca1d58ff-1ba1-4bc6-a7cf-2193184a334c/page/index.js
@@ -0,0 +1,178 @@
+window.__NEXT_REGISTER_PAGE('/', function() {
+  var comp = (module.exports = webpackJsonp(
+    [7],
+    {
+      234: function(e, a, t) {
+        e.exports = t(235)
+      },
+      235: function(e, a, t) {
+        'use strict'
+        function o(e) {
+          return e && e.__esModule ? e : { default: e }
+        }
+        Object.defineProperty(a, '__esModule', { value: !0 })
+        var r = t(11),
+          n = o(r),
+          s = t(2),
+          d = o(s),
+          i = t(3),
+          l = o(i),
+          c = t(12),
+          b = o(c),
+          u = t(13),
+          m = o(u),
+          x = t(40),
+          f = o(x),
+          p = t(0),
+          w = o(p),
+          h = t(29),
+          j = (o(h), t(41)),
+          k = o(j),
+          g = t(86),
+          E = o(g),
+          N = (function(e) {
+            function a(e) {
+              return (
+                (0, d.default)(this, a),
+                (0, b.default)(
+                  this,
+                  (a.__proto__ || (0, n.default)(a)).call(this, e)
+                )
+              )
+            }
+            return (
+              (0, m.default)(a, e),
+              (0, l.default)(a, [
+                {
+                  key: 'render',
+                  value: function() {
+                    return w.default.createElement(
+                      E.default,
+                      null,
+                      w.default.createElement(f.default, {
+                        styleId: '1787608278',
+                        css: [
+                          'h1.jsx-1787608278{padding-top:7rem;}',
+                          'a.jsx-1787608278{color:#f8114d !important;}',
+                          '.card.jsx-1787608278{width:auto;max-width:300px;margin:1.5rem auto 0;background-color:#fff;border:none;padding:2rem 1.5rem;box-shadow:0 3px 20px rgba(0,0,0,.1);}',
+                          '.btn-primary.jsx-1787608278,.btn-primary.jsx-1787608278:focus,.btn-primary.jsx-1787608278:active{background:#f8114d !important;border-color:#f8114d !important;}',
+                          'input.jsx-1787608278{max-width:200px;margin:0 auto;height:2.5rem;padding:0 1rem;border:0;border-radius:3px;background-color:#f4f4fb;-webkit-transition:border-color .25s ease-in-out,-webkit-box-shadow .5s;-webkit-transition:border-color .25s ease-in-out,-webkit-box-shadow .5s;transition:border-color .25s ease-in-out,-webkit-box-shadow .5s;-webkit-transition:box-shadow .5s,border-color .25s ease-in-out;transition:box-shadow .5s,border-color .25s ease-in-out;-webkit-transition:box-shadow .5s,border-color .25s ease-in-out,-webkit-box-shadow .5s;transition:box-shadow .5s,border-color .25s ease-in-out,-webkit-box-shadow .5s;-webkit-appearance:none;-moz-appearance:none;-webkit-appearance:none;-moz-appearance:none;appearance:none;}',
+                          'label.jsx-1787608278{color:#868f96 !important;}',
+                          'textarea.jsx-1787608278{resize:none;height:7.25rem;padding:0 1.125rem;padding-top:1.6rem;padding-bottom:1rem;border:0;border-radius:3px;background-color:#f4f4fb !important;border-radius:3px;line-height:1.5;-webkit-transition:border-color .25s ease-in-out,-webkit-box-shadow .5s;-webkit-transition:border-color .25s ease-in-out,-webkit-box-shadow .5s;transition:border-color .25s ease-in-out,-webkit-box-shadow .5s;-webkit-transition:box-shadow .5s,border-color .25s ease-in-out;transition:box-shadow .5s,border-color .25s ease-in-out;-webkit-transition:box-shadow .5s,border-color .25s ease-in-out,-webkit-box-shadow .5s;transition:box-shadow .5s,border-color .25s ease-in-out,-webkit-box-shadow .5s;-webkit-appearance:none;-moz-appearance:none;-webkit-appearance:none;-moz-appearance:none;appearance:none;}',
+                          '.form-input.jsx-1787608278:focus{border-color:#868f96;box-shadow:none;}',
+                          '#error.jsx-1787608278{color:red;display:none;}',
+                          '@media only screen and (max-width:1100px){.card.jsx-1787608278{width:100%;}h1.jsx-1787608278,h2.jsx-1787608278{font-size:1rem;}}'
+                        ]
+                      }),
+                      w.default.createElement(
+                        'div',
+                        { className: 'jsx-1787608278 container' },
+                        w.default.createElement(
+                          'h1',
+                          {
+                            className:
+                              'jsx-1787608278 card-title h1 text-center animated fadeInDown'
+                          },
+                          'Random generators'
+                        ),
+                        w.default.createElement(
+                          'div',
+                          { className: 'jsx-1787608278 columns' },
+                          w.default.createElement(
+                            'div',
+                            {
+                              className:
+                                'jsx-1787608278 column col-xs-12 col-6 text-center'
+                            },
+                            w.default.createElement(
+                              'div',
+                              {
+                                className:
+                                  'jsx-1787608278 card animated fadeInUp'
+                              },
+                              w.default.createElement(
+                                'div',
+                                { className: 'jsx-1787608278 card-header' },
+                                w.default.createElement(
+                                  'h2',
+                                  { className: 'jsx-1787608278 card-title h2' },
+                                  'Passwords generator'
+                                ),
+                                w.default.createElement(
+                                  'div',
+                                  {
+                                    className:
+                                      'jsx-1787608278 card-subtitle text-gray'
+                                  },
+                                  w.default.createElement(
+                                    k.default,
+                                    { href: '/passwords' },
+                                    w.default.createElement(
+                                      'a',
+                                      {
+                                        className: 'jsx-1787608278 btn btn-link'
+                                      },
+                                      'Generate now'
+                                    )
+                                  )
+                                )
+                              )
+                            )
+                          ),
+                          w.default.createElement(
+                            'div',
+                            {
+                              className:
+                                'jsx-1787608278 column col-xs-12 col-6 text-center'
+                            },
+                            w.default.createElement(
+                              'div',
+                              {
+                                className:
+                                  'jsx-1787608278 card animated fadeInUp'
+                              },
+                              w.default.createElement(
+                                'div',
+                                { className: 'jsx-1787608278 card-header' },
+                                w.default.createElement(
+                                  'h2',
+                                  { className: 'jsx-1787608278 card-title h2' },
+                                  'Usernames generator'
+                                ),
+                                w.default.createElement(
+                                  'div',
+                                  {
+                                    className:
+                                      'jsx-1787608278 card-subtitle text-gray'
+                                  },
+                                  w.default.createElement(
+                                    k.default,
+                                    { href: '/usernames' },
+                                    w.default.createElement(
+                                      'a',
+                                      {
+                                        className: 'jsx-1787608278 btn btn-link'
+                                      },
+                                      'Generate now'
+                                    )
+                                  )
+                                )
+                              )
+                            )
+                          )
+                        )
+                      )
+                    )
+                  }
+                }
+              ]),
+              a
+            )
+          })(w.default.Component)
+        a.default = N
+      }
+    },
+    [234]
+  ))
+  return { page: comp.default }
+})
